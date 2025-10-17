ec04063ef4b72de597bd373f48ee74dbe3c5bba5
jheddings <jason@heddings.com>
2025-10-16 19:51:20 -0600

    Better test coverage.

--- get_test.go
+++ get_test.go
@@ -1,7 +1,10 @@
 package cfprefs
 
 import (
+	"math/rand/v2"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestGetKeypath(t *testing.T) {
@@ -61,12 +64,301 @@ func TestGetKeypath(t *testing.T) {
 		t.Fatalf("failed to get map: %v", err)
 	}
 
-	retrievedMap, ok := value.(map[string]any)
-	if !ok {
-		t.Fatalf("value is not map[string]any: got %T", value)
+	if !reflect.DeepEqual(value, testValue) {
+		t.Fatalf("map does not match: expected %v, got %v", testValue, value)
+	}
+}
+
+func TestGetStr(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := "hello"
+	err := Set(appID, "str-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "str-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "str-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
 	}
 
-	if len(retrievedMap) != len(testValue) {
-		t.Fatalf("map length does not match: expected %d, got %d", len(testValue), len(retrievedMap))
+	if _, err = GetInt(appID, "str-test"); err == nil {
+		t.Fatal("expected error for non-int value, got nil")
+	}
+
+	value, err := GetStr(appID, "str-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != testValue {
+		t.Fatalf("expected %s, got %s", testValue, value)
+	}
+}
+
+func TestGetInt(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := rand.Int64()
+	err := Set(appID, "int-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "int-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "int-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetBool(appID, "int-test"); err == nil {
+		t.Fatal("expected error for non-bool value, got nil")
+	}
+
+	value, err := GetInt(appID, "int-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != testValue {
+		t.Fatalf("expected %d, got %d", testValue, value)
+	}
+}
+
+func TestGetFloat(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := rand.Float64()
+	err := Set(appID, "float-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "float-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "float-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetData(appID, "float-test"); err == nil {
+		t.Fatal("expected error for non-data value, got nil")
+	}
+
+	value, err := GetFloat(appID, "float-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != testValue {
+		t.Fatalf("expected %f, got %f", testValue, value)
+	}
+}
+
+func TestGetBool(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := true
+	err := Set(appID, "bool-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "bool-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "bool-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetDate(appID, "bool-test"); err == nil {
+		t.Fatal("expected error for non-date value, got nil")
+	}
+
+	value, err := GetBool(appID, "bool-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != testValue {
+		t.Fatalf("expected %t, got %t", testValue, value)
+	}
+}
+
+func TestGetDate(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := time.Now()
+	err := Set(appID, "date-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "date-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "date-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetMap(appID, "date-test"); err == nil {
+		t.Fatal("expected error for non-slice value, got nil")
+	}
+
+	value, err := GetDate(appID, "date-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != testValue {
+		t.Fatalf("expected %s, got %s", testValue.Format(time.RFC3339), value.Format(time.RFC3339))
+	}
+}
+
+func TestGetSlice(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := []any{rand.Int(), rand.Float64(), true, time.Now()}
+	err := Set(appID, "slice-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "slice-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "slice-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetStr(appID, "slice-test"); err == nil {
+		t.Fatal("expected error for non-map value, got nil")
+	}
+
+	value, err := GetSlice(appID, "slice-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(value, testValue) {
+		t.Fatalf("expected %v, got %v", testValue, value)
+	}
+}
+
+func TestGetData(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := []byte("hello world")
+	err := Set(appID, "data-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "data-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "data-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetFloat(appID, "data-test"); err == nil {
+		t.Fatal("expected error for non-float value, got nil")
+	}
+
+	value, err := GetData(appID, "data-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(value, testValue) {
+		t.Fatalf("expected %v, got %v", testValue, value)
+	}
+}
+
+func TestGetMap(t *testing.T) {
+	appID := "com.jheddings.cfprefs.testing"
+
+	testValue := map[string]any{
+		"string": "hello",
+		"number": rand.Int(),
+		"float":  rand.Float64(),
+		"bool":   false,
+		"time":   time.Now(),
+	}
+	err := Set(appID, "map-test", testValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := Delete(appID, "map-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	exists, err := Exists(appID, "map-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("expected true for existing key, got false")
+	}
+
+	if _, err = GetSlice(appID, "map-test"); err == nil {
+		t.Fatal("expected error for non-slice value, got nil")
+	}
+
+	value, err := GetMap(appID, "map-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(value, testValue) {
+		t.Fatalf("expected %v, got %v", testValue, value)
 	}
 }
